ab8ba4f42965ae9c5f9bde01b35fb796cbb382b1
hilaryakeba <2025bcs027@std.must.ac.ug>
2025-09-09 08:48:12 +0300

    Update VENDING MACHINE GRP 9.c

--- VENDING MACHINE GRP 9.c
+++ VENDING MACHINE GRP 9.c
@@ -14,14 +14,14 @@ int main () {
     char *name[]={"Soda", "Water", "Juice", "Crisps", "Chocolate"};
     int price[]={1500, 1000, 2000, 1200, 2500};
     //performing displaying
-    printf("🇺🇬💵💵WELCOME TO C VENDING MACHINE💵💵🇺🇬\n");
+    printf("=====WELCOME TO C VENDING MACHINE=====\n");
     printf("%-6s %-15s %-8s\n", "No.","Name","Price");
     printf("___    _________       ____________\n");
     for(int i=0 ; i<number;++i) {
     printf("%-6d %-15s %-8dUGX\n",i + 1,name[i], price[i]);
     }
-    printf("🇺🇬♥️♥️💵💵💵💵💵💵💵💵🌹💵💵💵💵💵💵💵♥️♥️🇺🇬\n");
-    printf("                \n");
+    printf("==========================================\n");
+    printf("\n");
     //ask for customer choice
     printf("Enter item number:");
     scanf("%d",& choice);
@@ -37,7 +37,7 @@ int main () {
        //performing calculation to findout totalPrice
        int totalprice=price[choice-1] * quantity;
        printf("Total Price: %d UGX\n", totalprice);
-       //ask user to  input 💰
+       //ask user to  input 
        printf("Enter amount to pay: ");
        scanf("%d", & payment);
        //calculation about change
@@ -47,19 +47,19 @@ int main () {
            printf("Your change is: %d UGX\n", change);
            printf("Purchase confirmed, dispense item\n");
        } else{
-           printf("Insufficient funds, please add more money %d\n", change);
+           printf("Insufficient funds, please add more money %d\n", -change);
                }
        //change break down
     int denom_number = 10;
     int denominations[] = {50000, 20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50};
     char *denom_names[] = {"50,000", "20,000", "10,000", "5,000", "2,000", "1,000", "500", "200", "100", "50"};
     //displaying the breakdown
-    printf("                                                 \n");
-    printf("🇺🇬♥️♥️💵💵💵💵💵💵💵💵💵🌹💵💵💵💵💵💵💵♥️♥️🇺🇬\n");
+    printf("\n");
+    printf("==================================================\n");
 
     printf("HERE'S YOUR CHANGE BREAK DOWN OF: %d UGX\n", change);
             printf("%-8s %-18s\n", "Main", "Denomination Name");
-            printf("                                   \n");
+            printf("\n");
     for(int i=0; i<denom_number; i++){
         int main= change / denominations[i];
         if (main > 0) {
@@ -68,7 +68,7 @@ int main () {
         }
     }
             printf("Thanks dear clients, pleased to serve you\n" );
-    printf("🇺🇬♥️♥️💵💵💵💵💵💵💵💵🌹💵💵💵💵💵💵💵💵♥️♥️🇺🇬");
+    printf("===================================================\n");
     return 0;
 
 
